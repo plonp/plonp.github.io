c0b86eebe327ea5dfe57861c099fde30a00585b4
plonp <40211693+plonp@users.noreply.github.com>
2024-10-07 23:51:46 +0800

    save

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-function myFunction() {
-	const _array = ["testing some stuff", "glorious purpose", "good shit"];
-	let _text = _array[Math.floor(Math.random() * _array.length)];
-	const _addText = document.getElementById('add');
-	_addText.innerHTML += _text + ", ";
-}
-
 let _textCount = 32;
 function counter() {
 	if (_textCount > 0) {
@@ -16,4 +9,4 @@ function counter() {
 		_textCount--;
 	}
 }
-setInterval(counter, 1000);
+setInterval(counter, 100);
