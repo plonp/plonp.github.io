b58407b1983842aecb15ef0b5a4a7e121601f5b8
plonp <40211693+plonp@users.noreply.github.com>
2024-10-08 00:10:22 +0800

    save

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let _textCount = 32;
 function counter() {
 	if (_textCount > 0) {
-		const _array = ["testing some stuff", "glorious purpose", "good shit"];
+		const _array = ["testing some stuff", "glorious purpose", "web learning"];
 		let _text = _array[Math.floor(Math.random() * _array.length)];
 		const _addText = document.getElementById('add');
 		_addText.innerHTML += _text + ", ";		
