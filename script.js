f4f11568994d64f8a7c3b95a7658c50d2a8eb13f
plonp <40211693+plonp@users.noreply.github.com>
2025-04-15 10:19:31 +0800

    save

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let _textCount = 32;
-function counter() {
-	if (_textCount > 0) {
-		const _array = ["testing some stuff", "glorious purpose", "web learning"];
-		let _text = _array[Math.floor(Math.random() * _array.length)];
-		const _addText = document.getElementById('add');
-		_addText.innerHTML += _text + ", ";
+// let _textCount = 32;
+// function counter() {
+// 	if (_textCount > 0) {
+// 		const _array = ["testing some stuff", "glorious purpose", "web learning"];
+// 		let _text = _array[Math.floor(Math.random() * _array.length)];
+// 		const _addText = document.getElementById('add');
+// 		_addText.innerHTML += _text + ", ";
 		
-		_textCount--;
-	}
-}
-setInterval(counter, 800);
+// 		_textCount--;
+// 	}
+// }
+// setInterval(counter, 800);
