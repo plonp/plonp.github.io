83f6e11ed36d222e9fe0bccce1545c50a51c5b86
plonp <40211693+plonp@users.noreply.github.com>
2025-12-07 23:09:27 +0800

    .

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 window.onload = function() {
 	// Library Containers
 		const _libButtons = ["/src/buttons/button.html"];
-	// Loader
-		const _body = document.querySelector('body');
 	// Fetch components
-		fetchData("/src/buttons/button.html");
+		fetchData(_libButtons[0]);
+	// Buttons
+		document.addEventListener('click', (e) => {
+			e.preventDefault();
+			switch (e.target.id) {
+				case "btn-html": case "btn-css":
+					console.log(e.target.parentElement.parentElement);
+					break;
+				default:
+					console.log(e.target);
+					break;
+			}
+		});
 }
 
 async function fetchData(_url) {
@@ -12,27 +22,27 @@ async function fetchData(_url) {
 		const response = await fetch(_url);
 		let data = await response.text(); //console.log('data : ', data);
 		data = data.replace('\r\n', '');
-		// Get HTML Code Block
-			const startIndex = data.indexOf('<body>'), endIndex = data.indexOf('</body>');
-			let codeHTML = data.substring(startIndex, endIndex).replace('<body>','');
-			console.log('codeHTML : ', JSON.stringify(codeHTML));
+
+		// Parse Fetched Data to HTML DOM
+			const parser = new DOMParser();
+			const HTMLData = parser.parseFromString(data, 'text/html'); //console.log('HTMLData : ' , HTMLData);
 
 		// Get Last Modified Date
 			let _date = new Date(response.headers.get('last-modified'));
 			_date = `${_date.getDate()}.${_date.getMonth()}.${_date.getFullYear()}`;
 			console.log(_date);
-		
-		const parser = new DOMParser();
-		const HTMLData = parser.parseFromString(data, 'text/html'); //console.log('HTMLData : ' , HTMLData);
 
-		// Load Code Preview
+		// Load HTML code block
+			const startIndex = data.indexOf('<body>'), endIndex = data.indexOf('</body>');
+			let codeHTML = data.substring(startIndex, endIndex).replace('<body>','');
+			console.log('codeHTML : ', JSON.stringify(codeHTML));
+
+		// Load CSS code block
 			const codeCSS = HTMLData.querySelector('style').textContent.replace('\n', '');
-
-			const htmlBlock = HTMLData.body; //console.log(htmlBlock.textContent);
 
 		const _component = HTMLData.querySelector('body');
 		const _codeBlock = document.querySelector('.component-code-block');
-		_codeBlock.textContent = `${codeCSS}`;
+		_codeBlock.textContent = `${codeHTML}`;
 
 		// Shadow DOM
 			const _compPreview = document.querySelector('.component-preview');
